ebf2473ec6e8b84c1ff97e6f673d5ec624416e4a
Suretha1520 <suretha2002@gmail.com>
2025-05-24 17:13:26 +0530

    Update: Made changes to [filename]

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Healthcare-Dashboard/', // 👈 Required for GitHub Pages to work
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
